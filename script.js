dc2fc054e9adc54c7988a29201ee5335c7cba78f
Ivaylo Moshekov <Ivaylo.Moshekov@digitall.com>
2024-10-11 16:34:50 +0300

    slice not supported?

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function addCharacter() {
     }
     const level = parseInt(document.getElementById('level').value);
     const isElite = document.getElementById('elite-monster').checked;
-    const entytyName = `${type} ${document.getElementById('standee-number').value.toLowerCase()}`;
-    let name =  entytyName.charAt(0).toUpperCase() + entytyName.slice(1);
+    let name = `${type} ${document.getElementById('standee-number').value.toLowerCase()}`;
+    // let name =  entytyName.charAt(0).toUpperCase() + entytyName.slice(1);
     // name = name.replaceAll('-', ' ');
     const monsterData = data.monsters.find(monster => monster.name === type);
     let selectedMonster = monsterData.stats[level];
