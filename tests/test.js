1617de467f3301b71309870bdf3cbe485b695fcb
Ivaylo Moshekov <Ivaylo.Moshekov@digitall.com>
2024-11-04 13:57:30 +0200

    added test to display attack modal

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const { Builder, By, until } = require('selenium-webdriver');
 const assert = require('assert');
-const sourceHTML = 'file:///' + __dirname + '/../index.html'; 
+const sourceHTML = 'file:///' + __dirname + '/../index.html';
 
 let driver;
 
@@ -11,6 +11,18 @@ async function setup() {
 
 async function tearDown() {
     await driver.quit();
+}
+
+async function addMonster(driver, monster) {
+    // Set up the inputs
+    await driver.findElement(By.id('type')).sendKeys(monster.type); // Set type
+    await driver.findElement(By.id('level')).clear();
+    await driver.findElement(By.id('level')).sendKeys(monster.level); // Set level
+    await driver.findElement(By.id('standee-number')).sendKeys(monster.standee); // Set standee number
+
+    // Click the "Add Monster" button
+    const addMonsterButton = await driver.findElement(By.css('.add-char button.initiative:nth-of-type(2)'));
+    await addMonsterButton.click();
 }
 
 async function testCreatureContainerHasContent() {
@@ -39,7 +51,7 @@ async function testAlertForMissingType() {
 }
 
 async function testAddMonster() {
-    let driver = await new Builder().forBrowser('chrome').build();
+    // let driver = await new Builder().forBrowser('chrome').build();
 
     try {
         // Load your local HTML file
@@ -47,16 +59,11 @@ async function testAddMonster() {
 
         const initialMonsters = await driver.findElements(By.css('.creature-row'));
         const initialCount = initialMonsters.length;
-
-        // Set up the inputs
-        await driver.findElement(By.id('type')).sendKeys('algox-guard'); // Set type
-        await driver.findElement(By.id('level')).clear();
-        await driver.findElement(By.id('level')).sendKeys('2'); // Set level
-        await driver.findElement(By.id('standee-number')).sendKeys('1'); // Set standee number
-
-        // Click the "Add Monster" button
-        let addMonsterButton = await driver.findElement(By.css('.add-char button.initiative:nth-of-type(2)'));
-        await addMonsterButton.click();
+        await addMonster(driver, {
+            type: 'algox-guard',
+            level: '2',
+            standee: '1'
+        });
 
         // Wait until a new creature is added to the DOM
         await driver.wait(async () => {
@@ -69,17 +76,50 @@ async function testAddMonster() {
         const finalCount = finalMonsters.length;
 
         if (finalCount > initialCount) {
-            console.log("Test passed: A new monster was successfully added.");
+            console.log("Test passed: a new monster was successfully added.");
+
         } else {
-            console.log("Test failed: No new monster was added.");
+            console.log("Test failed: no new monster was added.");
         }
 
     } catch (error) {
         console.error("Error:", error);
-    } finally {
-        await driver.quit(); // Ensure the browser closes after the test
     }
 }
+
+async function testAttackModalDisplay() {
+    try {
+        // Navigate to the page containing the HTML
+        await driver.get(sourceHTML); // Update this with the correct path
+        // Wait for the page to load
+        await addMonster(driver, {
+            type: 'algox-guard',
+            level: '2',
+            standee: '1'
+        });
+
+        // Find the 'attack-image' button for the non-aggressive character
+        const attackButton = await driver.findElement(By.id('attack-img-0')); // Adjust index as needed
+        await attackButton.click();
+
+        const targetButton = await driver.findElement(By.id('target-img-4')); // Adjust index as needed
+        await targetButton.click();
+
+        // Wait for the modal to be displayed
+        const modal = await driver.wait(until.elementLocated(By.id('modal-attack')), 5000);
+
+        // Validate that the modal is displayed
+        let isModalDisplayed = await modal.isDisplayed();
+        if (isModalDisplayed) {
+            console.log('Test passed: attack Modal is displayed as expected.');
+        } else {
+            console.log('Test failed: attack Modal is NOT displayed.');
+        }
+    } catch (error) {
+        console.error('Test failed:', error);
+    }
+}
+
 // Main function to run all tests sequentially
 async function runAllTests() {
     await setup();
@@ -88,6 +128,7 @@ async function runAllTests() {
         await testAlertForMissingType();
         await testCreatureContainerHasContent();
         await testAddMonster();
+        await testAttackModalDisplay();
     } catch (error) {
         console.error("Test failed:", error);
     } finally {
