19d4df5735e84785a0c7275ff685000ac1ed0346
Ivaylo <ivaylo.moshekov@gmail.com>
2024-11-04 17:01:32 +0200

    ui test for monster kill (#52)
    Co-authored-by: Ivaylo Moshekov <Ivaylo.Moshekov@digitall.com>

--- tests/test.js
+++ tests/test.js
@@ -22,10 +22,10 @@ async function tearDown() {
 }
 
 async function addMonster(driver, monster) {
-    await driver.findElement(By.id('type')).sendKeys(monster.type); 
+    await driver.findElement(By.id('type')).sendKeys(monster.type);
     await driver.findElement(By.id('level')).clear();
-    await driver.findElement(By.id('level')).sendKeys(monster.level); 
-    await driver.findElement(By.id('standee-number')).sendKeys(monster.standee); 
+    await driver.findElement(By.id('level')).sendKeys(monster.level);
+    await driver.findElement(By.id('standee-number')).sendKeys(monster.standee);
 
     // Click the "Add Monster" button
     const addMonsterButton = await driver.findElement(By.css('.add-char button.initiative:nth-of-type(2)'));
@@ -66,91 +66,112 @@ async function testAlertForMissingType() {
 }
 
 async function testAddMonster() {
-    try {
-        await driver.get(sourceHTML); 
+    await driver.get(sourceHTML);
 
-        const initialMonsters = await driver.findElements(By.css('.creature-row'));
-        const initialCount = initialMonsters.length;
+    const initialMonsters = await driver.findElements(By.css('.creature-row'));
+    const initialCount = initialMonsters.length;
 
-        await addMonster(driver, {
-            type: 'algox-guard',
-            level: '2',
-            standee: '1'
-        });
+    await addMonster(driver, {
+        type: 'algox-guard',
+        level: '2',
+        standee: '1'
+    });
 
-        // Wait until a new creature is added to the DOM
-        await driver.wait(async () => {
-            let currentMonsters = await driver.findElements(By.css('.creature-row'));
-            return currentMonsters.length > 0; 
-        }, 10000, "Timeout waiting for new monster to be added.");
+    // Wait until a new creature is added to the DOM
+    await driver.wait(async () => {
+        let currentMonsters = await driver.findElements(By.css('.creature-row'));
+        return currentMonsters.length > 0;
+    }, 10000, "Timeout waiting for new monster to be added.");
 
-        // Verify if a new creature has been added
-        const finalMonsters = await driver.findElements(By.css('.creature-row'));
-        const finalCount = finalMonsters.length;
+    // Verify if a new creature has been added
+    const finalMonsters = await driver.findElements(By.css('.creature-row'));
+    const finalCount = finalMonsters.length;
 
-        assert.ok(finalCount > initialCount, "no new monster was added.");
-        console.log("Test passed: a new monster was successfully added.");
-
-    } catch (error) {
-        console.error("Error:", error);
-    }
+    assert.ok(finalCount > initialCount, "no new monster was added.");
+    console.log("Test passed: a new monster was successfully added.");
 }
 
 async function testAttackModalDisplay() {
-    try {
-        await driver.get(sourceHTML); 
+    await driver.get(sourceHTML);
 
-        await addMonster(driver, {
-            type: 'algox-guard',
-            level: '2',
-            standee: '1'
-        });
+    await addMonster(driver, {
+        type: 'algox-guard',
+        level: '2',
+        standee: '1'
+    });
 
-        await openAttackModal();
+    await openAttackModal();
 
-        // Wait for the modal to be displayed
-        const modal = await driver.wait(until.elementLocated(By.id('modal-attack')), 5000);
+    // Wait for the modal to be displayed
+    const modal = await driver.wait(until.elementLocated(By.id('modal-attack')), 5000);
 
-        // Validate that the modal is displayed
-        let isModalDisplayed = await modal.isDisplayed();
+    // Validate that the modal is displayed
+    let isModalDisplayed = await modal.isDisplayed();
 
-        assert.ok(isModalDisplayed, "attack modal is NOT displayed.");
-        console.log('Test passed: attack modal is displayed as expected.');
-    } catch (error) {
-        console.error('Test failed:', error);
-    }
+    assert.ok(isModalDisplayed, "attack modal is NOT displayed.");
+    console.log('Test passed: attack modal is displayed as expected.');
 }
 
 async function testDamageApplication() {
-    try {
-        await driver.get(sourceHTML); 
-        await addMonster(driver, {
-            type: 'algox-guard',
-            level: '2',
-            standee: '1'
-        });
+    await driver.get(sourceHTML);
+    await addMonster(driver, {
+        type: 'algox-guard',
+        level: '2',
+        standee: '1'
+    });
 
-        let characterHPStat = await driver.findElement(By.id('char-hp-4'));
-        const originalHPValue = parseInt(await characterHPStat.getAttribute('value'));
+    let characterHPStat = await driver.findElement(By.id('char-hp-4'));
+    const originalHPValue = parseInt(await characterHPStat.getAttribute('value'));
 
-        await openAttackModal(0, 4);
+    await openAttackModal(0, 4);
 
-        const damageInput = await driver.findElement(By.id('attack-input'));
-        await damageInput.clear(); 
-        await damageInput.sendKeys('3'); 
+    const damageInput = await driver.findElement(By.id('attack-input'));
+    await damageInput.clear();
+    await damageInput.sendKeys('3');
 
-        // Click the "OK" button to apply damage
-        const applyDamageButton = await driver.findElement(By.css('#modal-attack .modal-content .initiative'));
-        await applyDamageButton.click();
+    // Click the "OK" button to apply damage
+    const applyDamageButton = await driver.findElement(By.css('#modal-attack .modal-content .initiative'));
+    await applyDamageButton.click();
 
-        // Retrieve the updated HP after damage is applied
-        const updatedHPValue = parseInt(await characterHPStat.getAttribute('value'));
+    // Retrieve the updated HP after damage is applied
+    const updatedHPValue = parseInt(await characterHPStat.getAttribute('value'));
 
-        assert.ok((originalHPValue - updatedHPValue) === 3, "damage is not applied correctly.");
-        console.log('Test passed: damage is applied correctly.');
-    } catch (error) {
-        console.error('Test failed:', error);
-    } 
+    assert.ok((originalHPValue - updatedHPValue) === 3, "damage is not applied correctly.");
+    console.log('Test passed: damage is applied correctly.');
+
+}
+
+async function testMonsterIsKilled() {
+    await driver.get(sourceHTML);
+    await addMonster(driver, {
+        type: 'algox-guard',
+        level: '1',
+        standee: '1'
+    });
+
+    const initialMonsters = await driver.findElements(By.css('.creature-row'));
+    const initialMonstersCount = initialMonsters.length;
+
+    let characterHPStat = await driver.findElement(By.id('char-hp-4'));
+    const originalHPValue = parseInt(await characterHPStat.getAttribute('value'));
+
+    await openAttackModal(0, 4);
+
+    const damageInput = await driver.findElement(By.id('attack-input'));
+    await damageInput.clear();
+    await damageInput.sendKeys(originalHPValue);
+
+    // Click the "OK" button to apply damage
+    const applyDamageButton = await driver.findElement(By.css('#modal-attack .modal-content .initiative'));
+    await applyDamageButton.click();
+
+    // Retrieve the updated HP after damage is applied
+    const updatedMonsters = await driver.findElements(By.css('.creature-row'));
+    const updatedMonstersCount = updatedMonsters.length;
+
+    assert.ok((initialMonstersCount - updatedMonstersCount) === 1, "monster has not been removed upon a kill");
+    console.log('Test passed: monster has successfully been killed and removed from the game.');
+
 }
 
 async function runAllTests() {
@@ -162,6 +183,7 @@ async function runAllTests() {
         await testAddMonster();
         await testAttackModalDisplay();
         await testDamageApplication();
+        await testMonsterIsKilled();
     } catch (error) {
         console.error("Test failed:", error);
     } finally {
