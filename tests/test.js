ce506cd6c680899469235684516248dca7ac5d73
Marian <marian.mmarinov@gmail.com>
2024-11-09 12:16:00 +0200

    i did the stupid

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,7 @@ const TestUtils  = require('./test-utils.js');
 
 let driver;
 
-const sourceHTML = !process.env.GITHUB_ACTIONS ?
+const sourceHTML = process.env.GITHUB_ACTIONS ?
     (process.env.SERVER_URL || 'http://127.0.0.1:8080') + '/index.html' :
     'file:///' + __dirname + '/../index.html'; // Adjust this path as needed
 
