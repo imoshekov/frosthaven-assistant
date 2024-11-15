aee583895f760f653ba123a228457d4cc3d539fd
Ivaylo <ivaylo.moshekov@gmail.com>
2024-11-15 15:11:24 +0200

    Ws refactor + element sync bug (#92)
    * wsclient refactor
    
    * server.js refactored
    
    * fixed element sync
    
    ---------
    
    Co-authored-by: Ivaylo Moshekov <Ivaylo.Moshekov@digitall.com>

--- scripts/server.js
+++ scripts/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const PORT = process.env.PORT || 8080;
 
-const sessions = {}; 
+const sessions = {};
 let characters = [
     { name: "Bonera Bonerchick", type: "boneshaper", aggressive: false, hp: 7, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {}, defaultStats: { hp: 6, attack: 0, movement: 0, initiative: 0 } },
     { name: "Spaghetti", type: "drifter", aggressive: false, hp: 12, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {}, defaultStats: { hp: 10, attack: 0, movement: 0, initiative: 0 } },
@@ -67,6 +67,7 @@ wss.on('connection', (ws) => {
                 isNewSession: isNewSession
             }));
             if (sessions[sessionId].length > 1) {
+                //update clients with current state of synced elemenets
                 ws.send(JSON.stringify({ type: 'characters-update', characters: characters }));
                 ws.send(JSON.stringify({ type: 'round-update', roundNumber: roundNumber }));
                 Object.keys(elementStates).forEach(elementId => {
@@ -80,41 +81,15 @@ wss.on('connection', (ws) => {
         }
         if (data.type === 'characters-update') {
             characters = data.characters;
-            if (currentSessionId && sessions[currentSessionId]) {
-                sessions[currentSessionId].forEach(client => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: 'characters-update', characters: characters }));
-                    }
-                });
-            }
-        }
+            broadcastToSession(currentSessionId, 'characters-update', { characters });
+        }       
         if (data.type === 'round-update') {
-            roundNumber = data.roundNumber; 
-            if (currentSessionId && sessions[currentSessionId]) {
-                sessions[currentSessionId].forEach(client => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({
-                            type: 'round-update',
-                            roundNumber: data.roundNumber
-                        }));
-                    }
-                });
-            }
-        }
+            roundNumber = data.roundNumber;
+            broadcastToSession(currentSessionId,'round-update', { roundNumber: data.roundNumber });
+        }       
         if (data.type === 'element-update') {
-            const { elementId, elementState } = data;
-            elementStates[elementId] = elementState;
-            // Broadcast the color update to all clients in the same session
-            if (currentSessionId && sessions[currentSessionId]) {
-                sessions[currentSessionId].forEach(client => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({
-                            type: 'element-update',
-                            elementState: data.elementState
-                        }));
-                    }
-                });
-            }
+            elementStates[data.elementId] = data.elementState;
+            broadcastToSession(currentSessionId, 'element-update', { elementState: data.elementState });
         }
     });
 
@@ -127,6 +102,16 @@ wss.on('connection', (ws) => {
         }
     });
 });
+
+function broadcastToSession(sessionId, type, data) {
+    if (sessionId && sessions[sessionId]) {
+        sessions[sessionId].forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ type, ...data }));
+            }
+        });
+    }
+}
 
 // Set up a regular interval to check if clients are still alive
 setInterval(() => {
--- scripts/wsclient.js
+++ scripts/wsclient.js
@@ -4,9 +4,11 @@ const WebSocketHandler = {
 
     initialize: async function () {
         try {
-            this.ws = new WebSocket('wss://frosthaven-assistant.onrender.com');
-            //local debug, todo to change this dynamically
-            // this.ws = new WebSocket('ws://localhost:8080');
+            this.ws = new WebSocket(
+                window.location.hostname.includes('github.io') ?
+                    'wss://frosthaven-assistant.onrender.com'
+                    : 'ws://localhost:8080'
+            );
 
             this.ws.onopen = () => {
                 const sessionId = prompt("Enter session ID or leave blank to create a new one:");
@@ -28,40 +30,28 @@ const WebSocketHandler = {
 
             this.ws.onmessage = (event) => {
                 const data = JSON.parse(event.data);
-
-                if (data.type === 'session-joined') {
-                    const message = `session: ${data.sessionId}, ${data.clientsCount} client(s) connected.`;
-                    document.getElementById('session-id').textContent = message;
-                    const toast = document.getElementById('toast-notification');
-                    toast.textContent = message;
-                    toast.classList.add('show');
-
-                    setTimeout(() => {
-                        toast.classList.remove("show");
-                        toast.classList.add("hide");        
-                    }, 3000);
-                }
-                if (data.type === 'characters-update') {
-                    characters = data.characters;
-                    UIController.renderTable();
-                }
-                if (data.type === 'round-update') {
-                    document.getElementById('round-number').value = data.roundNumber;
-                }
-                if (data.type === 'element-update') {
-                    const elementState = JSON.parse(data.elementState)
-                    const element = document.getElementById(elementState.elementId);
-
-                    if (element) {
-                        const pathElement = element.querySelector('path');
-                        pathElement.setAttribute('d', elementState.path); 
-                        pathElement.setAttribute('fill', elementState.fill); 
+                switch (data.type) {
+                    case "session-joined": {
+                        this.handleSessionJoined(data);
+                        break;
+                    }
+                    case "characters-update": {
+                        this.handleCharacterUpdate(data);
+                        break;
+                    }
+                    case "round-update": {
+                        this.handleRoundUpdate(data);
+                        break;
+                    }
+                    case "element-update": {
+                        this.handleElementUpdate(data);
+                        break;
                     }
                 }
             };
 
         } catch (error) {
-            console.error("Error waking up the server:", error.message);
+            console.error("WebSocketHandler error", error.message);
         }
     },
     getInstance: function () {
@@ -79,10 +69,40 @@ const WebSocketHandler = {
             roundNumber: roundNumberValue
         }));
     },
-    sendElementState: function (elementState) {
+    sendElementState: function (elementId, elementState) {
         this.ws.send(JSON.stringify({
             type: 'element-update',
-            elementState: elementState,
+            elementId: elementId,
+            elementState: elementState, // Send as an object
         }));
+    },
+    handleSessionJoined: function (data) {
+        const message = `session: ${data.sessionId}, ${data.clientsCount} client(s) connected.`;
+        document.getElementById('session-id').textContent = message;
+        const toast = document.getElementById('toast-notification');
+        toast.textContent = message;
+        toast.classList.add('show');
+
+        setTimeout(() => {
+            toast.classList.remove("show");
+            toast.classList.add("hide");
+        }, 3000);
+    },
+    handleCharacterUpdate: function (data) {
+        characters = data.characters;
+        UIController.renderTable();
+    },
+    handleRoundUpdate: function (data) {
+        document.getElementById('round-number').value = data.roundNumber;
+    },
+    handleElementUpdate: function (data) {
+        const elementState = JSON.parse(data.elementState)
+        const element = document.getElementById(elementState.elementId);
+
+        if (element) {
+            const pathElement = element.querySelector('path');
+            pathElement.setAttribute('d', elementState.path);
+            pathElement.setAttribute('fill', elementState.fill);
+        }
     }
 };
