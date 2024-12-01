983a90560a9b89d899ecf9dec1c49632f3ac48e5
Marian <marian.mmarinov@gmail.com>
2024-12-01 12:49:45 +0200

    WIP2

--- scripts/battle-log.js
+++ scripts/battle-log.js
@@ -1,16 +1,19 @@
 class Log {
-    static ACTION = Object.freeze({
-        hp: 'hp',
+    static PART = Object.freeze({
+        hp: 'HP',
+        add: 'Add',
+        set: 'Set', // manually set HP
         init: 'init',
-        attack: 'attack',
+        attack: 'Attack',
+        result: 'Result',
         shield: 'shield',
         brittle: 'brittle',
         retaliate: 'retaliate',
         poison: 'poison',
-        wound: 'wound',
+        wound: 'Wound',
         ward: 'ward',
-        heal: 'heal',
-        die: 'die'
+        heal: 'Heal', // Heal not yet implemented, but added for future use
+        die: 'Die'
     });
 
     logParts = {};
@@ -23,50 +26,130 @@ class Log {
         this.logParts = logParts;
     }
 
+    toJON() {
+        return this.logParts;
+    }
+
+    /*
+     * Print Format:
+     * [HP]: [Add/Attack/Wound/Heal] ([Died]) [Shield] [Poison] [Brittle] [Ward] = [Result] ([Retaliate]), [Initiative]
+     */
+    print() {
+        const hpMsg = `${this.logParts[Log.PART.hp] || '??'}: `;
+        const deathMsg = this.logParts[Log.PART.die] ? `(Died) ` : '';
+        const dmgModifiersMsg = this.getModifiers();
+        const initiativeMsg = this.logParts[Log.PART.init] || '99';
+
+        return `${hpMsg}${deathMsg}${this.getAction()}${this.getModifiers()}${this.getAttackResult()}, ${initiativeMsg}`;
+    }
+
+    getAction() {
+        const log = this.logParts;
+        switch (true) {
+            case Log.PART.add in log:
+                return Log.PART.add; // adding always has full HP
+            case Log.PART.set in log:
+                return Log.PART.set; // set to custom value
+            case Log.PART.attack in log:
+                return Log.PART.attack + ' ' + log[Log.PART.attack];
+            case Log.PART.heal in log:
+                return Log.PART.heal + ' ' + log[Log.PART.heal];
+            case Log.PART.wound in log:
+                return Log.PART.wound + ' ' + log[Log.PART.wound];
+            default:
+                return '??';
+        }
+    }
+
+    getModifiers() {
+        const log = this.logParts;
+        let msgParts = [];
+        if (Log.PART.shield in log) {
+            msgParts.push('<img src="images/fh/action/shield.svg"> ' + log[Log.PART.shield]);
+        }
+        if (Log.PART.poison in log) {
+            msgParts.push('<img src="images/fh/condition/poison.svg"> 1');
+        }
+        if (Log.PART.brittle in log) {
+            msgParts.push('<img src="images/fh/condition/brittle.svg"> x2');
+        }
+        if (Log.PART.ward in log) {
+            msgParts.push('<img src="images/fh/condition/ward.svg"> /2');
+        }
+        const modifiers = msgParts.join(', ');
+
+        return modifiers ? ` [${modifiers}]` : '';
+    }
+
+    getAttackResult() {
+        const log = this.logParts;
+        let resultParts = [];
+        if (Log.PART.result in log) {
+            resultParts.push(' = '  + log[Log.PART.result]);
+        }
+        if (Log.PART.retaliate in log) {
+            resultParts.push('(<img src="images/fh/action/retaliate.svg"/>' + log[Log.PART.retaliate] + ')');
+        }
+
+        return resultParts.join(' ');
+    }
+
     static #logBuilder = {
         logParts: {},
         hp(value) {
-            this.logParts[Log.ACTION.hp] = value;
+            this.logParts[Log.PART.hp] = value;
             return this;
         },
-        init(value) {
-            this.logParts[Log.ACTION.init] = value;
+        add(value) {
+            this.logParts[Log.PART.add] = value;
+            return this;
+        },
+        set(value) {
+            this.logParts[Log.PART.set] = value;
+            return this;
+        },
+        initiative(value) {
+            this.logParts[Log.PART.init] = value;
             return this;
         },
         attack(value) {
-            this.logParts[Log.ACTION.attack] = value;
+            this.logParts[Log.PART.attack] = value;
+            return this;
+        },
+        result(value) {
+            this.logParts[Log.PART.result] = value;
             return this;
         },
         shield(value) {
-            this.logParts[Log.ACTION.shield] = value;
+            this.logParts[Log.PART.shield] = value;
             return this;
         },
         brittle(value) {
-            this.logParts[Log.ACTION.brittle] = value;
+            this.logParts[Log.PART.brittle] = value;
             return this;
         },
         retaliate(value) {
-            this.logParts[Log.ACTION.retaliate] = value;
+            this.logParts[Log.PART.retaliate] = value;
             return this;
         },
         poison(value) {
-            this.logParts[Log.ACTION.poison] = value;
+            this.logParts[Log.PART.poison] = value;
             return this;
         },
         wound(value) {
-            this.logParts[Log.ACTION.wound] = value;
+            this.logParts[Log.PART.wound] = value;
             return this;
         },
         ward(value) {
-            this.logParts[Log.ACTION.ward] = value;
+            this.logParts[Log.PART.ward] = value;
             return this;
         },
         heal(value) {
-            this.logParts[Log.ACTION.heal] = value;
+            this.logParts[Log.PART.heal] = value;
             return this;
         },
         die(value) {
-            this.logParts[Log.ACTION.die] = value;
+            this.logParts[Log.PART.die] = value;
             return this;
         },
 
@@ -97,7 +180,7 @@ class Log {
         // Populate the sidebar content
         const sidebarContent = document.getElementById('battle-log-content');
         const imgId = target.id;
-        sidebarContent.innerHTML = `<p>Logs for ${imgId}</p>`;
+        sidebarContent.innerHTML = `<p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p><p>Logs for ${imgId}</p>`;
         target.style.visibility = 'hidden';
         this.lastLogClicked = target;
     }
@@ -116,5 +199,6 @@ class Log {
             this.lastLogClicked = null;
         }
     }
+
     //endregion
 }
