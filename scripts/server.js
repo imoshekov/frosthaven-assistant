0654f7ca89ebb3ed6380b4ed8a440ac2db1966da
Marian <marian.mmarinov@gmail.com>
2024-12-01 18:53:52 +0200

    new char

--- scripts/server.js
+++ scripts/server.js
@@ -9,7 +9,8 @@ const sessions = {};
 const defaultCharacters = [
     { name: "Bonera Bonerchick", type: "boneshaper", aggressive: false, hp: 8, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {},tempStats: {} },
     { name: "Spaghetti", type: "drifter", aggressive: false, hp: 12, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {},tempStats: {}},
-    { name: "Bufalina", type: "banner-spear", aggressive: false, hp: 12, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {},tempStats: {}},
+    // RIP { name: "Bufalina", type: "banner-spear", aggressive: false, hp: 12, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {},tempStats: {}},
+    { name: "Krio Pat'ra", type: "snowflake", aggressive: false, hp: 11, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {},tempStats: {}},
     { name: "Petra Squirtenstein", type: "deathwalker", aggressive: false, hp: 9, attack: 0, movement: 0, initiative: 0, armor: 0, retaliate: 0, conditions: {},tempStats: {} }
 ];
 
