1ad45469928b90ef474d420f894da32ea365afcb
Ivaylo Moshekov <ivaylo.moshekov@gmail.com>
2025-08-21 17:52:09 +0300

    test browser compatibility

---
+++ frosthaven-assistant-angular/src/polyfills.ts
@@ -0,0 +1,29 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS for LG TV (WebOS 2018 ~ Chrome 60-65)
+ **************************************************************************************************/
+
+// Core ES polyfills
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/weak-map';
+import 'core-js/es/weak-set';
+import 'core-js/es/reflect';
+import 'core-js/es/promise';
+import 'core-js/es/typed-array';
+
+// Async/await support
+import 'regenerator-runtime/runtime';
+
+// Angular dependency
+import 'zone.js';  // Included with Angular
