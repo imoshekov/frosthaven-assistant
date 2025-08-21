3f4e4880a48cb880ff6c72c96683d90ec902ce62
Ivaylo Moshekov <ivaylo.moshekov@gmail.com>
2025-08-21 18:15:42 +0300

    more TV compatibility

--- frosthaven-assistant-angular/src/polyfills.ts
+++ frosthaven-assistant-angular/src/polyfills.ts
@@ -1,29 +1,20 @@
 /***************************************************************************************************
- * BROWSER POLYFILLS for LG TV (WebOS 2018 ~ Chrome 60-65)
- **************************************************************************************************/
+ * BROWSER POLYFILLS
+ */
 
-// Core ES polyfills
-import 'core-js/es/symbol';
+/** ES5+ polyfills via core-js */
+import 'core-js/es/array';
 import 'core-js/es/object';
 import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
 import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
+import 'core-js/es/number';
 import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/set';
-import 'core-js/es/weak-map';
-import 'core-js/es/weak-set';
-import 'core-js/es/reflect';
 import 'core-js/es/promise';
-import 'core-js/es/typed-array';
 
-// Async/await support
-import 'regenerator-runtime/runtime';
+/** Optional: classList support for older SVG elements */
+import 'classlist.js';
 
-// Angular dependency
-import 'zone.js';  // Included with Angular
+/** Angular required zone.js */
+import 'zone.js';
