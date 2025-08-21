98285aed3539f2f5b91dcefa459a79faa2ced848
Ivaylo Moshekov <ivaylo.moshekov@gmail.com>
2025-08-21 23:34:03 +0300

    try babel

---
+++ frosthaven-assistant-angular/src/polyfills.ts
@@ -0,0 +1,18 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// ES5 polyfills via core-js
+import 'core-js/es/object';
+import 'core-js/es/array';
+import 'core-js/es/function';
+import 'core-js/es/string';
+import 'core-js/es/number';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/promise';
+
+// Optional DOM/polyfills
+import 'classlist.js';  // SVG element.classList support
+import 'zone.js';       // Required by Angular
