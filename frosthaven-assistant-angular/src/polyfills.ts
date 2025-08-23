7abe06607509c26aed3435b86d1ad3ea41897876
Ivaylo <ivaylo.moshekov@gmail.com>
2025-08-23 13:09:54 +0300

    test chrome68 part 4

--- frosthaven-assistant-angular/src/polyfills.ts
+++ frosthaven-assistant-angular/src/polyfills.ts
@@ -4,6 +4,19 @@ import 'core-js/es/promise';
 import 'core-js/es/string';
 import 'zone.js'; // Required by Angular
 
+import 'core-js/actual/promise';
+import 'core-js/actual/array/from';
+import 'core-js/actual/array/includes';
+import 'core-js/actual/object/assign';
+import 'core-js/actual/object/entries';
+import 'core-js/actual/object/values';
+import 'core-js/actual/url-search-params';
+import 'core-js/actual/string/starts-with';
+import 'core-js/actual/string/ends-with';
+// If you use animations:
+import 'web-animations-js';
+
+
 // Add missing flatMap for environments like Chromium 68
 if (!Array.prototype.flatMap) {
   (Array.prototype as any).flatMap = function(callback: any, thisArg?: any) {
